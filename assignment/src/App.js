92f53babc38d075b3c7b0dace2405ee720d2e9a5
yde <philip.ydehed@gmail.com>
2020-11-04 11:33:21 +0100

    first commit

---
+++ assignment/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Route, Switch } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      Group 4 Assignment
+      <Switch>
+
+        <Route>
+          
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
