f820ff45337b6ed6d8b6c37b90107da6b737f7fd
Jonas Gertz <Geertzo@live.se>
2020-11-04 17:44:19 +0100

    removed console log

--- assignment/src/components/Signup/SignupForm.jsx
+++ assignment/src/components/Signup/SignupForm.jsx
@@ -25,7 +25,6 @@ export default function SignupForm() {
     setSignupFormData({...signupFormData, [e.target.name]: e.target.value})
    
   }
-  console.log(signupFormData);
 
   return (
     <div className={styles.signupForm}>
