4ffa44b6485b3fc939671adff2d0d03c63f90f6b
Jonas Gertz <Geertzo@live.se>
2020-11-05 16:46:29 +0100

    fixed account created bug

--- assignment/src/components/Signup/ActivateAccount.jsx
+++ assignment/src/components/Signup/ActivateAccount.jsx
@@ -6,14 +6,15 @@ export default function ActivateAccount(props) {
   const TOKEN = props.URLdata.get('token')
   const [UIDdata, setUIDdata] = useState(UID)
   const [tokenData, setTokenData] = useState(TOKEN)
+  const [accountedCreated, setAccountCreated] = useState(false)
 
   const userKit = new UserKit()
 
   function handleOnClickActivateAccount() {
-    console.log(UIDdata, tokenData);
     userKit.activateAccount(UIDdata, tokenData)
-    .then(res => res.json())
-    .then(data => console.log(data))
+    .then(res => {
+      res.status === 204 ? setAccountCreated(true) : setAccountCreated(false)
+    })
   }
 
   return (
@@ -31,6 +32,12 @@ export default function ActivateAccount(props) {
             onChange={e => setTokenData(e.target.value)}
           />
           <button onClick={handleOnClickActivateAccount}>Activate account</button>
+          {accountedCreated && (
+            <p>Account created! woh</p>
+          )}
+          {!accountedCreated && (
+            <p>Account not activated yet</p>
+          )}
         </>
       )}
     </div>
